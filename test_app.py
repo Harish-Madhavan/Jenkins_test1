d157137f69a651eb22ad15e252e0f6fee2b3c723
Harish-Madhavan <veeharish2005@gmail.com>
2026-09-23 12:08:34 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,13 +1,13 @@
 import unittest
 # Import your functions from app.py
-from app import your_function_name 
+from app import get_message 
 
 # Your test code MUST be wrapped in a class inheriting from unittest.TestCase
 class TestMyApplication(unittest.TestCase):
     
     # Every test function name MUST start with the prefix "test_"
     def test_application_output(self):
-        result = your_function_name()
+        result = get_message()
         self.assertEqual(result, "Expected Output")
 
 if __name__ == '__main__':
