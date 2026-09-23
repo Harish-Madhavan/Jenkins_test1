8394d4dc14fa3d34a8a03b61c527ee2d730e805f
Harish-Madhavan <veeharish2005@gmail.com>
2026-09-23 12:06:42 +0530

    update

--- test_app.py
+++ test_app.py
@@ -1,4 +1,14 @@
-from app import get_message
+import unittest
+# Import your functions from app.py
+from app import your_function_name 
 
-def test_get_message():
-    assert get_message() == "Hello from Windows Jenkins!"
+# Your test code MUST be wrapped in a class inheriting from unittest.TestCase
+class TestMyApplication(unittest.TestCase):
+    
+    # Every test function name MUST start with the prefix "test_"
+    def test_application_output(self):
+        result = your_function_name()
+        self.assertEqual(result, "Expected Output")
+
+if __name__ == '__main__':
+    unittest.main()
